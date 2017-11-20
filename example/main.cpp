0e5921f05c8b0797b04fdef724e679e3ab0565d6
Alexis Paques <alexis.paques@gmail.com>
2017-11-20 16:40:50 +0100

    Reindentation

--- example/main.cpp
+++ example/main.cpp
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <tchar.h>
 #include <iostream>
 #include "untar/untar.h"
 
@@ -8,52 +7,52 @@
 
 using namespace untar;
 
-int main(int argc, char **argv) {
-								tarFile* file;
-								++argv; /* Skip program name */
-								if (*argv != NULL) {
-																file = new tarFile(*argv, File);
-								}
+int main(int argc, char** argv)
+{
+    tarFile* file;
+    ++argv; /* Skip program name */
+    if (*argv != NULL) {
+        file = new tarFile(*argv, File);
+    }
 
-								// We now have all the files ready to be extracted.
-								// If we want to find a file called Mommy.txt, use the map.find method
-								// First define the key you are looking for. This is the filename in the tar
-								// For example : myFolder/Mommy.txt
-								string key = "myFolder/Mommy.txt";
+    // We now have all the files ready to be extracted.
+    // If we want to find a file called Mommy.txt, use the map.find method
+    // First define the key you are looking for. This is the filename in the tar
+    // For example : myFolder/Mommy.txt
+    string key = "myFolder/Mommy.txt";
 
-								// Find the file itself
-								auto it = file->entries.find(key);
+    // Find the file itself
+    auto it = file->entries.find(key);
 
-								// If the file we found is NOT the end of the map (which means we did not find it), then extract the data !
-								if (it != file->entries.end())
-								{
-																tarEntry * data = it->second;
+    // If the file we found is NOT the end of the map (which means we did not find it), then extract the data !
+    if (it != file->entries.end()) {
+        tarEntry* data = it->second;
 
-																// Get the ifstream, startbyte and filesize in one call
-																size_t start;
-																int filesize;
-																ifstream * _tarfile = data->wantToExtract(&filesize, &start);
+        // Get the ifstream, startbyte and filesize in one call
+        size_t start;
+        int filesize;
+        ifstream* _tarfile = data->wantToExtract(&filesize, &start);
 
-																// NOTE : file->_tarfile.is_open() is the same as the one in the entries.
-																// tarFile::_tarfile will be private in future release, it is for debugging reason
+        // NOTE : file->_tarfile.is_open() is the same as the one in the entries.
+        // tarFile::_tarfile will be private in future release, it is for debugging reason
 
-																// CARE THE OVERFLOW, ALWAYS
-																char dataoutput[500];
+        // CARE THE OVERFLOW, ALWAYS
+        char dataoutput[500];
 
-																// always be sure the file is open before trying to read. Else we got shitty random data.
-																if (_tarfile->is_open()) {
+        // always be sure the file is open before trying to read. Else we got shitty random data.
+        if (_tarfile->is_open()) {
 
-																								// Go to the beginning of the file and read the whole file
-																								_tarfile->seekg(data->getStartingByte(), ios_base::beg);
+            // Go to the beginning of the file and read the whole file
+            _tarfile->seekg(data->getStartingByte(), ios_base::beg);
 
-																								// Read the data
-																								_tarfile->read(dataoutput, filesize);
+            // Read the data
+            _tarfile->read(dataoutput, filesize);
 
-																								// Show the result
-																								cout << string(dataoutput, filesize);
-																}
-								}
-								system("pause");
+            // Show the result
+            cout << string(dataoutput, filesize);
+        }
+    }
+    system("pause");
 
-								return (0);
+    return (0);
 }
--- example/wrapped_find.cpp
+++ example/wrapped_find.cpp
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <tchar.h>
 #include <iostream>
 #include "untar/untar.h"
 
@@ -7,32 +6,33 @@
 
 using namespace untar;
 
-int main(int argc, char **argv) {
-								tarFile* file;
-								++argv; /* Skip program name */
-								if (*argv != NULL) {
-																file = new tarFile(*argv, File);
-								}
+int main(int argc, char** argv)
+{
+    tarFile* file;
+    ++argv; /* Skip program name */
+    if (*argv != NULL) {
+        file = new tarFile(*argv, File);
+    }
 
-								// We now have all the files ready to be extracted.
-								// If we want to find a file called Mommy.txt, use the map.find method
-								// First define the key you are looking for. This is the filename in the tar
-								// For example : myFolder/Mommy.txt
-								string key = "myFolder/Mommy.txt";
+    // We now have all the files ready to be extracted.
+    // If we want to find a file called Mommy.txt, use the map.find method
+    // First define the key you are looking for. This is the filename in the tar
+    // For example : myFolder/Mommy.txt
+    string key = "myFolder/Mommy.txt";
 
-								size_t start;
-								int filesize;
-								ifstream * filestream = file->find(key, &filesize, &start);
-								if (filestream != nullptr) {
-																if (filestream->is_open()) {
-																								char dataoutput[500];
-																								filestream->seekg(start, ios_base::beg);
-																								filestream->read(dataoutput, filesize);
-																								cout << string(dataoutput, filesize);
-																}
-								}
+    size_t start;
+    int filesize;
+    ifstream* filestream = file->find(key, &filesize, &start);
+    if (filestream != nullptr) {
+        if (filestream->is_open()) {
+            char dataoutput[500];
+            filestream->seekg(start, ios_base::beg);
+            filestream->read(dataoutput, filesize);
+            cout << string(dataoutput, filesize);
+        }
+    }
 
-								system("pause");
+    system("pause");
 
-								return (0);
+    return (0);
 }
